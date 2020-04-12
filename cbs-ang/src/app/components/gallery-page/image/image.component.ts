e825c03502dbb2bfa298ddd68547ccff446b1fbd
Ren Jeka <renjeka@gmail.com>
2020-04-12 19:41:16 +0300

    fix styles

--- cbs-ang/src/app/components/gallery-page/image/image.component.ts
+++ cbs-ang/src/app/components/gallery-page/image/image.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+import {GetGalleryService} from '../../../shared/get-gallery.service';
+
+// export interface PhotoImage {
+//   url: string;
+// }
+
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private serviceGetImage: GetGalleryService
+  ) { }
+
+  photo;
+
+  goBack() {
+    this.router.navigate(['gallery']);
+  }
 
   ngOnInit(): void {
+    //console.log(this.activatedRoute);
+    this.activatedRoute.params.forEach(paramUrl => {
+      const imgID = +paramUrl.imageId;
+
+      // console.log(paramUrl);
+      // console.log(imgID);
+
+      this.serviceGetImage.getImg(imgID).subscribe(responce => {
+        this.photo = responce;
+        console.log(this.photo);
+      });
+    });
   }
+
 
 }
--- cbs-ang/src/app/shared/get-gallery.service.ts
+++ cbs-ang/src/app/shared/get-gallery.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+// import {PhotoImage} from '../components/gallery-page/image/image.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetGalleryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getImages() {
+    return this.http.get('http://jsonplaceholder.typicode.com/photos?_limit=20');
+  }
+
+  getImg(pageId) {
+    return this.http.get(`http://jsonplaceholder.typicode.com/photos/${pageId}`);
+  }
 }
