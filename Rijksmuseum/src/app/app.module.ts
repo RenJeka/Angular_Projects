295f4e536bd383f6a9bb331da82e34557d13e467
RenJeka <renjeka@gmail.com>
2020-07-30 16:53:16 +0300

    • first design of popup component • deleted unused directory

--- Rijksmuseum/src/app/app.module.ts
+++ Rijksmuseum/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { PopupComponent } from './popup/popup.component';
 import { DetailsComponent } from './details/details.component';
 import {HttpClientModule} from "@angular/common/http";
 import { NgForWithNumbersDirective } from './shared/ng-for-with-numbers.directive';
-import { OverlayHoverDirective } from './shared/overlay-hover.directive';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,6 @@ import { OverlayHoverDirective } from './shared/overlay-hover.directive';
     PopupComponent,
     DetailsComponent,
     NgForWithNumbersDirective,
-    OverlayHoverDirective
   ],
   imports: [
     BrowserModule,
