9a7870969fa132325b9843d031d481e4bac5306b
renjeka <renjeka@gmail.com>
2024-10-10 19:47:56 +0300

    added 'tour-of-hero-capasitor' project

---
+++ tour-of-hero-capasitor/android/app/src/main/java/com/angularprojects/tourofherocapasitor/MainActivity.java
@@ -0,0 +1,5 @@
+package com.angularprojects.tourofherocapasitor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ tour-of-hero-capasitor/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.angularprojects.tourofherocapasitor',
+  appName: 'tour-of-hero-capasitor',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
